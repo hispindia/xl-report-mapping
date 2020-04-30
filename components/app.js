bff5fe6816e9d7fed1041d3e53648b750b7909ce
SamtaPandey <samta.pandey@hispindia.org>
2020-04-30 18:07:35 +0530

    dynamic route in xl-report-mapping

--- components/app.js
+++ components/app.js
@@ -5,24 +5,50 @@ import {ReportList} from './reportList'
 import {EditReportForm} from './editReportFormX'
 import {NewReportForm} from './newReportForm'
 
+import dhis2API from '../lib/dhis2API'
+
+
+
 export function App(props){
     
     var instance = Object.create(React.Component.prototype)
-  
-  
-    instance.render = function(){
+    var state = {
+        reportName : [],
+        count: 0
+    };
+    function getRout() {
         var baseName = props.baseURL.split("/")
+
         baseName = baseName[baseName.length-1];
-        
-        return (  <Router basename={baseName+"/api/apps/xl-report-mapping/index.html#"} >                 
-                  <Switch>
-                  <Route exact path="/reports"  component={ReportList}  />
-                  <Route exact path="/reports/add"  component={NewReportForm}  />
-                  <Route path="/reports/edit/"  component={EditReportForm}  />
+        dhis2API.getManifest().then(function (data) {
+            if(state.count ===0){
+                console.log("data.name: " + data.name);
+                state.reportName = data.name;
+                state.count = state.count + 1;
+                instance.setState(Object.assign({}, state));
+            }
+        })
+        if (state.count !== 0){
+        console.log(state.reportName);
+        return(<Router basename={baseName+"/api/apps/"+state.reportName+"/index.html#"} >
+            <Switch>
+                <Route exact path="/reports"  component={ReportList}  />
+                <Route exact path="/reports/add"  component={NewReportForm}  />
+                <Route path="/reports/edit/"  component={EditReportForm}  />
 
-                  </Switch>
+            </Switch>
 
-                  </Router>
+        </Router>)
+        }
+
+    }
+
+    instance.render = function(){
+
+        return (
+            <div>
+            {getRout()}
+            </div>
                )
     }
     return instance
